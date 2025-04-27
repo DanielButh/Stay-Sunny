ee50db0833d3de81739c5535fc670cf3a6a15a4c
tam <tania_islas001@comunidad.unam.mx>
2025-04-27 17:01:07 -0600

    Añadiendo estructura del API

---
+++ app/src/main/java/com/example/staysunny/core/RetrofitInstance.kt
@@ -0,0 +1,23 @@
+package com.example.staysunny.core
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitInstance {
+
+    object RetrofitInstance {
+        fun getRetrofit(): Retrofit {
+            val httpClient = OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl("https://www.weatherapi.com/")
+                .client(httpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/staysunny/model/Weather.kt
@@ -0,0 +1,3 @@
+package com.example.staysunny.model
+
+data class Weather()
