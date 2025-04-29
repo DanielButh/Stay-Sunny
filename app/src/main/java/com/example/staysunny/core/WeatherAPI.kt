b9b205736ab32727944bc03f1eeb96b8b96fc530
sebas0-0 <sebastianbarrbonilla.123@gmail.com>
2025-04-29 17:50:00 -0600

    conexión de WeatherAPI

--- app/src/main/java/com/example/staysunny/core/WeatherAPI.kt
+++ app/src/main/java/com/example/staysunny/core/WeatherAPI.kt
@@ -1,5 +1,6 @@
 package com.example.staysunny.core
 
+import com.example.staysunny.model.Weather
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,7 +9,7 @@ interface WeatherAPI {
 
     @GET("current.json") //Este es el endpoint que pide el clima actual en WeatherAPI.
     suspend fun getCurrentWeather(
-        @Query("key") apiKey: String,  //Tu clave de API que te da weatherapi.com.
+        @Query("") apiKey: String,  //Tu clave de API que te da weatherapi.com.
         @Query("q") location: String   // La ciudad o coordenada que quieres buscar.
     ): Response<Weather>   //Respuesta del JSON.
 
