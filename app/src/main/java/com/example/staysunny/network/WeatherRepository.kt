266baee5735747cd6627cfccd60397f875cb8bf7
Danbuth <buthdan10@gmail.com>
2025-04-29 22:22:52 -0600

    Permission, WeatherFragment y WeatherViewModel

--- app/src/main/java/com/example/staysunny/network/WeatherRepository.kt
+++ app/src/main/java/com/example/staysunny/network/WeatherRepository.kt
@@ -8,10 +8,10 @@ import com.example.staysunny.model.Weather
 class WeatherRepository {
     private var retrofit = RetrofitInstance.getRetrofit().create(WeatherAPI::class.java)
 
-    suspend fun getCurrentWeather(): Weather? {
-        val reponse = retrofit.getCurrentWeather("ccbb8f761b5b493cb1b232630252904", "19.330410742165437, -99.21020365058398")
-        Log.i("RESPONSE", reponse.body().toString())
+    suspend fun getWeatherDetail(coordinates: String): Weather? {
+        val response = retrofit.getWeatherInfo("c5d316da89f8417aae562221251904", coordinates)
+        Log.i("RESPONSE", response.body().toString())
 
-        return reponse.body()
+        return response.body()
     }
 }
